b9137d5dbbd2975d55171bc2b75d38a690c95c55
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-04-16 10:05:01 +0100

    restructure; link to generated assets

--- web/index.php
+++ web/index.php
@@ -2,13 +2,16 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->register(new SilexAssetic\AsseticServiceProvider());
 
-$app['assetic.path_to_web'] = __DIR__;
-$app['assetic.options'] = array(
-    'debug' => true,
-);
+$app['assetic.path_to_web']     = __DIR__;
+$app['assetic.path_to_cache']   = __DIR__ . '/../cache/assetic' ;
+$app['assetic.options']         = array(
+                                        'debug' => true,
+                                    );
+// Setup the filter manager
 $app['assetic.filter_manager'] = $app->share(
     $app->extend('assetic.filter_manager', function($fm, $app) {
         $fm->set('scssphp', new Assetic\Filter\ScssphpFilter());
@@ -16,6 +19,8 @@ $app['assetic.filter_manager'] = $app->share(
         return $fm;
     })
 );
+
+// Setup the asset manager
 $app['assetic.asset_manager'] = $app->share(
     $app->extend('assetic.asset_manager', function($am, $app) {
 
@@ -25,7 +30,7 @@ $app['assetic.asset_manager'] = $app->share(
                 __DIR__ . '/../vendor/jlong/sass-twitter-bootstrap/lib/bootstrap.scss',
                 array($app['assetic.filter_manager']->get('scssphp'))
             ),
-            new Assetic\Cache\FilesystemCache(__DIR__ . '/cache/assetic')
+            new Assetic\Cache\FilesystemCache( $app['assetic.path_to_cache'] )
         ));
         $am->get('styles')->setTargetPath('css/styles.css');
 
@@ -35,7 +40,7 @@ $app['assetic.asset_manager'] = $app->share(
                 __DIR__ . '/../vendor/jlong/sass-twitter-bootstrap/js/*.js',
                 array( $app['assetic.filter_manager']->get('jsmin') )
                 ),
-                new Assetic\Cache\FilesystemCache(__DIR__ . '/cache/assetic')
+                new Assetic\Cache\FilesystemCache( $app['assetic.path_to_cache'] )
             )
         );
         $am->get('javascripts')->setTargetPath('js/javascripts.js');
@@ -44,8 +49,15 @@ $app['assetic.asset_manager'] = $app->share(
     })
 );
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-  return 'Hello '.$app->escape($name);
+$app->get('/hello', function () use ($app) {
+    $html = '<html><head>'
+        . '<link rel="stylesheet" href="css/styles.css">'
+        . '<script src="js/javascripts.js"></script>'
+        . '</head><body>'
+        . '<p>Silex Assetic test</p>'
+        . '</body></html>';
+
+  return $html;
 });
 
 $app->run();
